5f070c16c0093a280c8b725cd41d7abbaa9b5db5
Jakob Mattsson <jakob.mattsson@gmail.com>
2018-07-04 08:55:21 +0100

    More data

--- exec.sh
+++ exec.sh
@@ -10,6 +10,8 @@ csvtojson data/channelfireball.csv > data/channelfireball.json
 
 
 
-# Cant remember what these were used for
-# node allData.js > data/ix_all_cards.csv
-# csvjoin --left -c "Card,title,card" data/ix_nice.csv data/channelfireball.csv data/ix_all_cards.csv | csvcut -C title,card > data/ix_enhanced.csv
+
+# Produce the rated file
+csvjoin --left -c title data/channelfireball.csv data/rarities.csv | csvcut -c title,imageUrl,lowScore,highScore,review,color,set,rarity > data/channelfireball_rarity.csv
+csvtojson data/channelfireball_rarity.csv > data/channelfireball_rarity.json
+
