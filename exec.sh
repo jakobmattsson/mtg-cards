d0a645a59970834c4babfd43e62d5de244d4375e
Jakob Mattsson <jakob.mattsson@gmail.com>
2018-07-04 08:13:46 +0100

    Added blue reviews

--- exec.sh
+++ exec.sh
@@ -1,5 +1,15 @@
-node getReview.js > data/channelfireball.csv
-csvtojson data/channelfireball.csv > data/channelfireball.json
+
+# Download the new CSV and merge it with the current one (could it be made so it doesn't add duplicates?)
+node getReview.js > data/channelfireball-new.csv
+csvstack data/channelfireball.csv data/channelfireball-new.csv | grep -Ev "^$" > data/channelfireball-all-temp.csv
+mv data/channelfireball-all-temp.csv data/channelfireball.csv
+rm data/channelfireball-new.csv
+
+
+# join with the rarities
+# csvtojson data/channelfireball.csv > data/channelfireball-new.json
+
+
 
 # Cant remember what these were used for
 # node allData.js > data/ix_all_cards.csv
