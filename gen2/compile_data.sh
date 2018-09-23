ef523afe4a11c8f96b08333a88aabc9a03fc539e
Jakob Mattsson <jakob.mattsson@gmail.com>
2018-09-23 13:14:21 +0200

    Added compiled data

--- gen2/compile_data.sh
+++ gen2/compile_data.sh
@@ -1,2 +1,7 @@
-csvtojson data/raw/grn.csv > data/compiled/json/grn.json
-cat data/compiled/json/grn.json > data/compiled/all.json
+csvtojson data/raw/dom.csv > data/compiled/sets/dom.json
+csvtojson data/raw/grn.csv > data/compiled/sets/grn.json
+csvtojson data/raw/m19.csv > data/compiled/sets/m19.json
+csvtojson data/raw/rix.csv > data/compiled/sets/rix.json
+csvtojson data/raw/xln.csv > data/compiled/sets/xln.json
+
+ls data/compiled/sets | xargs node -e "_ = require('lodash');console.log(JSON.stringify(_.flatten(process.argv.slice(1).map((x) => require('./data/compiled/sets/' + x)))))" > data/compiled/all.json
