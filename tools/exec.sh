94cace0d7050dd63fd91594dc22c9467c3c600bf
Jakob Mattsson <jakob.mattsson@gmail.com>
2018-09-27 21:53:46 +0100

    Updated all GRN

--- tools/exec.sh
+++ tools/exec.sh
@@ -2,16 +2,15 @@
 
 # Script. Antag att det finns en fil med alla kort (jas)
 
-
-JOINFILE="data/raw/jas.csv"
-OUTFILE="data/raw/grn.csv"
+JOINFILE="data/raw/jas.csv" # ratings will be removed
+OUTFILE="data/raw/grn.csv" # ratings and review will be extracted
 
 mkdir -p tmp
 node tools/downloadReviews/getReview.js > tmp/lsv-ratings.csv # get the reviews as configured in "getReview"
 cat tmp/lsv-ratings.csv | csvcut -c title,lowScore,highScore,review > tmp/lsv-min.csv
 cat $JOINFILE | csvcut -C lowScore,highScore > tmp/raw.csv
 csvjoin --right -c title tmp/lsv-min.csv tmp/raw.csv | csvcut -C 1 > $OUTFILE # output as 
-
+npm run build
 
 
 
